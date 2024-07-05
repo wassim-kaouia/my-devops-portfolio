b79f7771285900c941078ad7895a3bfe0646f33a
wassim-kaouia <wassim.elkaouia@gmail.com>
2024-07-05 09:23:55 +0100

    change footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Skote.
+                <script>document.write(new Date().getFullYear())</script> © WassDev.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Themesbrand
+                    Design & Develop by Wassim Kaouia | WassDev
                 </div>
             </div>
         </div>
