049b3d121a019050b2830693363b32cf44e7fb78
wassim-kaouia <wassim.elkaouia@gmail.com>
2024-07-05 09:24:25 +0100

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Wassim Kaouia | WassDev
+                    Designed & Developed by Wassim Kaouia | WassDev
                 </div>
             </div>
         </div>
